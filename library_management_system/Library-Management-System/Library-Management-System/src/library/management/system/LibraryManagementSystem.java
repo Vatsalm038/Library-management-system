f33f2da72946ec544bf1fa7e42dcd49dbf69991c
VATSAL MEHTA <91190459+Vatsalm038@users.noreply.github.com>
2022-03-01 23:01:22 +0530

    Add files via upload

---
+++ library_management_system/Library-Management-System/Library-Management-System/src/library/management/system/LibraryManagementSystem.java
@@ -0,0 +1,50 @@
+package library.management.system;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class LibraryManagementSystem extends JFrame implements ActionListener{
+
+        JLabel l1;
+        JButton b1;
+        
+        public LibraryManagementSystem() {
+		
+                setSize(500,500);
+                setLayout(null);
+                setLocation(200,150);
+
+		l1 = new JLabel("");
+                b1 = new JButton("Next");
+                
+                b1.setBackground(Color.WHITE);
+                b1.setForeground(Color.BLACK);
+				
+                
+                
+                ImageIcon i1  = new ImageIcon(ClassLoader.getSystemResource("library/management/system/icons/first1.jpg"));
+                Image i3 = i1.getImage().getScaledInstance(500, 500,Image.SCALE_DEFAULT);
+                ImageIcon i2 = new ImageIcon(i3);
+                l1 = new JLabel(i2);
+                
+                b1.setBounds(250,375,150,50);
+		l1.setBounds(0, 0, 500, 500);
+                
+                l1.add(b1);
+		add(l1);
+                
+                b1.addActionListener(this);
+	}
+        
+        public void actionPerformed(ActionEvent ae){
+                new Login_user().setVisible(true);
+                this.setVisible(false);
+                
+        }
+        
+        public static void main(String[] args) {
+                LibraryManagementSystem window = new LibraryManagementSystem();
+                window.setVisible(true);			
+	}
+}
---
+++ library_management_system/Library-Management-System/Library-Management-System/src/library/management/system/conn.java
@@ -0,0 +1,19 @@
+package library.management.system;
+
+import java.sql.*;
+
+public class conn {
+    Connection c;
+    Statement s;
+
+    public conn() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///vas", "root", "mehta123");
+            s = c.createStatement();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
